963f08456c74e0ae4fc85bfdda2a8c661891356d
DanhPhuongNha-22130191 <22130191@st.hcmuaf.edu.vn>
2026-01-12 07:13:04 +0700

    Add histogram equalization functionality for grayscale images
    - Implemented `histCalc` function to compute normalized histogram for grayscale images.
    - Developed `histEqual` function to perform histogram equalization using the computed histogram.
    - Added example usage with a sample image array and commented out code for reading an image from file.
    - Included necessary imports for NumPy and OpenCV.

---
+++ tuan3/tuan3.py
@@ -0,0 +1,65 @@
+import numpy as np
+import cv2
+
+def read_img(path):
+    gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    if gray is None:
+        print('Image is None')
+        return
+    return gray
+
+def histCalc(gray,L):
+   hist = np.zeros(L,dtype=np.float32)
+   M,N = gray.shape
+   for row in range(M):
+       for col in range(N):
+           g = gray[row,col]
+           if 0<=g<L:
+            hist[g]+=1
+   hist/=gray.size
+   return hist
+
+def histEqual(gray, L):
+    hist = histCalc(gray,L)
+    e_hist = np.zeros_like(hist)
+    for g in range(L):
+        e_hist[g] = hist[:g+1].sum()
+    c_max = 1
+    c_min = 0
+    for g in range(L):
+        if e_hist[g] > 0:
+            c_min = e_hist[g]
+            break
+    if c_max - c_min == 0:
+       return gray.copy()
+    else:
+        coef = (L-1)/(c_max-c_min)
+    e_hist = np.round((e_hist-c_min)*coef)
+    e_hist = e_hist.astype(np.uint8)
+    out = np.zeros_like(gray,dtype=np.uint8)
+    M,N = gray.shape
+    for row in range(M):
+        for col in range(N):
+            g = int(gray[row,col])
+            out[row,col] = e_hist[g]
+    return out
+
+img = np.array([
+    [12,  4, 16,  8, 10, 14, 16, 10],
+    [12,  4, 16,  8, 10, 14, 16, 10],
+    [ 4, 16, 10,  8, 16, 14, 16, 10],
+    [ 4, 10, 10,  4, 16, 14, 10,  4],
+    [ 4, 10, 16,  4, 10, 10, 10,  4],
+    [12,  4, 16,  4, 10, 10, 16, 16],
+    [12,  4, 10,  8, 10,  4, 16, 12],
+    [12,  4, 10,  8, 10,  4, 16, 12],
+])
+L=256
+print(histCalc(img,L))
+print(histEqual(img,L))
+    
+
+# path = r'tuan3\\black-white.png'
+# print(histCalc(read_img(path),256))
+
+    
